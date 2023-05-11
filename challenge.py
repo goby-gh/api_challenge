1bb8779b994c39d40f6848a907c29197fca3e52e
Noah Maze <noah@heliohome.io>
2023-05-10 22:09:05 -0500

    initial commit of auc result db model

--- challenge.py
+++ challenge.py
@@ -1,61 +1,28 @@
 # 2021-01-08
 # Backend technical challenge
 #
+from api import fetch_todays_results
+from models
 
-'''
-Intro:
+# This is SILLY (since this app doesn't serve anything)
+# but its the quickest way I know of to get sqlalch and alembic up and running and I'm working as fast as I can!
+from flask import Flask, request
+from flask_migrate import Migrate
 
-Habitat Energy submit their batteries into a frequency service called dynamic
-containment each day. The service operates as an auction where you submit your price
-and you find out if you are accepted or not.
 
-The results of the auction can be seen on the national grid ESO data page
-https://data.nationalgrideso.com/ancillary-services/dynamic-containment-data#. There is
-an API for this data -> click on tender results -> API.
-'''
 
-def fetch_auction_result():
-    ''' Read the auction result (via the API)
-    '''
-    # Integrate this dataset using cURL (from the bottom of https://data.nationalgrideso.com/ancillary-services/dynamic-containment-data#)
-    # # Get a list of dataset's resources
-    # curl -L -s https://data.nationalgrideso.com/ancillary-services/dynamic-containment-data/datapackage.json | grep path
-    #
-    # # Get resources
-    # curl -L https://data.nationalgrideso.com/backend/datastore/dump/6fd8e042-be27-4c67-ad59-5acdd2a7b0fd
-    #
-    # curl -L https://data.nationalgrideso.com/backend/datastore/dump/26aefbcc-fce0-403f-80e1-4a26af3fe84b
-    #
-    # curl -L https://data.nationalgrideso.com/backend/datastore/dump/ddc4afde-d2bd-424d-891c-56ad49c13d1a
-    #
-    # curl -L https://data.nationalgrideso.com/backend/datastore/dump/888e5029-f786-41d2-bc15-cbfd1d285e96
-    #
-    # curl -L https://data.nationalgrideso.com/backend/dataset/aca07dcb-f807-409c-a4ec-da5dc052b8ba/resource/0b8dbc3c-e05e-44a4-b855-7dd1aa079c68/download/dynamic_containment_masterdata.csv
-    pass
+# Intro:
+#
+# Habitat Energy submit their batteries into a frequency service called dynamic
+# containment each day. The service operates as an auction where you submit your price
+# and you find out if you are accepted or not.
+#
+# The results of the auction can be seen on the national grid ESO data page
+# https://data.nationalgrideso.com/ancillary-services/dynamic-containment-data#. There is
+# an API for this data -> click on tender results -> API.
 
-def save_current_days_result():
-    ''' Save Habitat's current day's result to a local database.
 
-        > The database model and testing database you use is up to you.
-        > Also this might not be the only data we get from ESO,
-        > so think about your code structure.
-    '''
-    pass
+# Read the auction result (via the API)
+results = fetch_todays_results()
 
-'''
-Additional info:
-
-    > To complete the task we must be able to run your code to see that it works (try not to
-    > make any assumptions about the operating system), so please add documentation to
-    > explain how it works.
-    >
-    > Try not to spend more than 3 hours on this task. If the task is going to take you longer
-    > than 3 hours, please send us what you have and write in the documentation what you
-    > would do next. Please send us your code as a compressed archive e.g. zip, tar.gz, bzip2
-    > etc.
-    >
-    > Happy coding
-    > Habitat Energy Tech team
-'''
-
-print("Hello world")
+# Save the results
--- models.py
+++ models.py
@@ -1,1 +1,97 @@
 # Sqlalchemy db models
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+bulk_data_schema = 'bulk_data'
+
+class AuctionResult(db.Model):
+    __tablename__ = 'auction_results'
+    __table_args__ = {'schema': 'noah' }
+    # Ours
+    auction_result_id = db.Column(db.BigInteger, primary_key=True)
+    created_at  = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp())
+    modified_at = db.Column(
+        db.DateTime,
+        default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+    # From API
+    dc_id = db.Column(db.Integer)
+    dc_count = db.Column(db.Integer, nullable=True)
+    company = db.Column(db.Text, nullable=True)
+    unit_name = db.Column(db.Text, nullable=True)
+    service = db.Column(db.Text, nullable=True)
+    technology_type = db.Column(db.Text, nullable=True)
+    location = db.Column(db.Text, nullable=True)
+    cancelled = db.Column(db.Text, nullable=True)
+    efa_date = db.Column(db.Numeric(12,6), nullable=True)
+    cleared_volume = db.Column(db.Numeric(12,6), nullable=True)
+    clearing_price = db.Column(db.Numeric(12,6), nullable=True)
+    delivery_start = db.Column(db.DateTime, nullable=True)
+    delivery_end = db.Column(db.DateTime, nullable=True)
+    dc_full_text = db.Column(db.Text, nullable=True)
+    efa = db.Column(db.Date, nullable=True)
+    def __str__(self):
+        return f"Address<{self.address_id}>"
+# Notes:
+# Fields from the API:
+# fields = [
+#     {'id': '_count', 'type': 'int8'},
+#     {'id': '_id', 'type': 'int4'},
+#     {'id': '_full_text', 'type': 'tsvector'},
+#     {'id': 'Company', 'type': 'text'},
+#     {'id': 'Unit Name', 'type': 'text'},
+#     {'id': 'EFA Date', 'type': 'date'},
+#     {'id': 'Delivery Start', 'type': 'timestamp'},
+#     {'id': 'Delivery End', 'type': 'timestamp'},
+#     {'id': 'EFA', 'type': 'numeric'},
+#     {'id': 'Service', 'type': 'text'},
+#     {'id': 'Cleared Volume', 'type': 'numeric'},
+#     {'id': 'Clearing Price', 'type': 'numeric'},
+#     {'id': 'Technology Type', 'type': 'text'},
+#     {'id': 'Location', 'type': 'text'},
+#     {'id': 'Cancelled', 'type': 'text'}
+# ]
+
+
+example_records = [
+    {   'Cancelled': '',
+        'Cleared Volume': '39',
+        'Clearing Price': '3',
+        'Company': 'HABITAT ENERGY LIMITED',
+        'Delivery End': '2023-05-10T03:00:00',
+        'Delivery Start': '2023-05-09T23:00:00',
+        'EFA': '1',
+        'EFA Date': '2023-05-10',
+        'Location': 'PR2',
+        'Service': 'DCH',
+        'Technology Type': 'Batteries',
+        'Unit Name': 'AG-HEL00G',
+        '_count': '35',
+        '_full_text': "'-05':8,11,17 '-09':12 '-10':9,18 "
+        "'00':14,15,20,21 '1':22 '2023':7,10,16 '3':25 "
+        "'39':24 'ag':5 'ag-hel00g':4 'batteri':26 "
+        "'dch':23 'energi':2 'habitat':1 'hel00g':6 "
+        "'limit':3 'pr2':27 't03':19 't23':13",
+        '_id': 250778},
+    {   'Cancelled': '',
+        'Cleared Volume': '16',
+        'Clearing Price': '0.9',
+        'Company': 'HABITAT ENERGY LIMITED',
+        'Delivery End': '2023-05-10T07:00:00',
+        'Delivery Start': '2023-05-10T03:00:00',
+        'EFA': '2',
+        'EFA Date': '2023-05-10',
+        'Location': 'OL113HA',
+        'Service': 'DCL',
+        'Technology Type': 'Batteries',
+        'Unit Name': 'AG-HEL0AG',
+        '_count': '35',
+        '_full_text': "'-05':8,11,17 '-10':9,12,18 '0.9':25 "
+        "'00':14,15,20,21 '16':24 '2':22 '2023':7,10,16 "
+        "'ag':5 'ag-hel0ag':4 'batteri':26 'dcl':23 "
+        "'energi':2 'habitat':1 'hel0ag':6 'limit':3 "
+        "'ol113ha':27 't03':13 't07':19",
+        '_id': 250789}]
